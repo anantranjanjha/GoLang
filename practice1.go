bb507a477bd2490b4e7245246ac4e57301c49a9d
Anant <anantranjanjha@gmail.com>
2025-06-06 17:29:15 +0530

    drop

--- practice1.go
+++ practice1.go
@@ -2,11 +2,35 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/GoesToEleven/puppy"
 )
 
+const x = 99
+
+func init() {
+	fmt.Println("This is my func do%d", x)
+}
+
 func main() {
-	fmt.Println("Hello Go!")
-	fmt.Println(puppy.BigBarks())
+
+	switch {
+	case x < 100:
+		fmt.Println("Lower")
+	case x > 100:
+		fmt.Println("Hiogher")
+	}
+	for x := 0; x < 100; x++ {
+		fmt.Printf("%d", x)
+	}
+
+	z := []int{10, 20, 30}
+	m := map[string]int{
+		"Hello": 41,
+		"gELLO": 56,
+	}
+	for i, v := range z {
+		fmt.Println(i, v)
+	}
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
 }
